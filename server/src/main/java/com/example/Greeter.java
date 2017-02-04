7a4757c41bcade473e93fbaebcdd289fd024888f
Octavian Tuchila <octaviantuchila14@gmail.com>
2017-02-04 18:06:49 +0000

    Added final to String

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   *TODO: Add javadoc comment
+   *returns String
+   */
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
